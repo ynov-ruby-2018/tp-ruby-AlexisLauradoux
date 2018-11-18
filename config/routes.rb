b16db30aec4622533ae6c140581ed3456b0a7144
Guillaume Barillot <gbarillot@gmail.com>
2018-11-18 18:10:44 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
-  resources :posts
+  resources :posts # Vous avez des routes ouvertes sur lesquelles rien n'écoute. Manque only: [:index...]
 end
